de07187e528b49b094865e2a976a3fa2601883e3
495057454@qq.com <495057454@qq.com>
2019-03-28 15:38:45 +0800

    【更新】删除账号密码

--- crawler/zhihu.py
+++ crawler/zhihu.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 import requests
 
@@ -6,6 +7,9 @@ headers = {
     'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36",
 }
 session = requests.Session()
+
+global rankIndex
+rankIndex = ""
 
 
 def juejin_login(phonenumber, password):  # 登陆掘金
@@ -25,8 +29,8 @@ def juejin_login(phonenumber, password):  # 登陆掘金
     return login_json
 
 
-def juejin_index(login_json):  # 爬取掘金首页内容
-
+def juejin_index(login_json, pams_rankIndex):  # 爬取掘金首页内容
+    global rankIndex
     index_data = {
         'src': 'web',
         'uid': login_json['user']['uid'],
@@ -34,6 +38,7 @@ def juejin_index(login_json):  # 爬取掘金首页内容
         'token': login_json['token'],
         'limit': '20',
         'category': 'all',
+        'before': pams_rankIndex,
         'recomment': '1',
     }
     index_response = session.get('https://timeline-merger-ms.juejin.im/v1/get_entry_by_rank?',
@@ -41,8 +46,53 @@ def juejin_index(login_json):  # 爬取掘金首页内容
                                  params=index_data)
     print(index_response.status_code)
     print("index_response:" + index_response.text)
+    json_index_response = json.loads(index_response.text)
+    entrylist = json_index_response['d']['entrylist']
+    print(entrylist)
+    sql_data_list = []  # 最后储存到数据的变量
+
+    for i, item in enumerate(entrylist):
+        if i + 1 == 20:
+            rankIndex = item['rankIndex']
+        sql_dict = {}
+        tag_title = item['tags'][0]['title']
+        print("文章类别：" + tag_title)
+        username = item['user']['username']
+        print("用户名：" + username)
+        jobTitle = item['user']['jobTitle']
+        print("大厂title：" + jobTitle)
+        contentTitle = item['title']
+        print("文章标题：" + contentTitle)
+        summaryInfo = item['summaryInfo']
+        print("文章简介：" + summaryInfo)
+        collectionCount = item['collectionCount']
+        print("文章收藏数：" + str(collectionCount))
+        print("-------------------------------------------------")
+        sql_dict["username"] = username
+        sql_dict["tag_title"] = tag_title
+        sql_dict["jobTitle"] = jobTitle
+        sql_dict["contentTitle"] = contentTitle
+        sql_dict["summaryInfo"] = summaryInfo
+        sql_dict["collectionCount"] = collectionCount
+        sql_data_list.append(sql_dict)
+        print("---------------------打印sql_data_list列表----------------------------")
+        # print(sql_data_list)
+        return sql_data_list
+
+
+def saveToLocalJson(sql_data):  # 保存到json文件中
+    json_data = json.dumps(sql_data)  # 将 Python 对象编码成 JSON 字符串
+    txt_file_path = os.path.split(os.path.realpath(__file__))[0] + "/sql.json"  # 这是保存txt文件的位置
+    print("数据存储路径：" + txt_file_path)
+    file = open(txt_file_path, 'w')
+    file.write(json_data)
+    file.close()
 
 
 if __name__ == '__main__':
-    json_dict = juejin_login('18159020779', 'cb8585204')
-    juejin_index(json_dict)
+    json_dict = juejin_login('XXX', 'XXX')  # 登陆成功
+    page = 0  # 初始化页数，TOP一共有250部   每页25部
+    while page <= 20:
+        page += 20
+        sql_data_list = juejin_index(json_dict, rankIndex)  # 获取sql_data_list列表
+        saveToLocalJson(sql_data_list)
